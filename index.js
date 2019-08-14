bfd236c28fd85d06e254a10ac0003028a4391f3b
Eoin Shanaghy <eoin.shanaghy@fourtheorem.com>
2019-08-14 14:02:23 +0100

    Handle Promise or callback Lambdas

--- index.js
+++ index.js
@@ -1,69 +1,89 @@
-'use strict'
+'use strict';
 
-const AWS = require('aws-sdk')
+const AWS = require('aws-sdk');
+const once = require('once');
 
-const sqs = new AWS.SQS({ endpoint: process.env.SQS_ENDPOINT_URL })
+const sqs = new AWS.SQS({endpoint: process.env.SQS_ENDPOINT_URL});
 
-const DLQ_QUEUE_NAME = process.env.DLQ_QUEUE_NAME
-const MAX_MESSAGES = process.env.DLQ_RETRY_MAX_MESSAGES || 10
+const DLQ_QUEUE_NAME = process.env.DLQ_QUEUE_NAME;
+const MAX_MESSAGES = process.env.DLQ_RETRY_MAX_MESSAGES || 10;
 
-const queueUrlPromise = sqs
-  .getQueueUrl({
-    QueueName: DLQ_QUEUE_NAME
-  })
-  .promise()
-  .then(({ QueueUrl: queueUrl }) => {
-    log.debug({ queueUrl }, 'Using queue URL')
-    return queueUrl
-  })
-
-function dlqHandlerCreator({ handler, log = console }) {
-  return retry
+function dlqHandlerCreator({handler, log = console}) {
+  const queueUrlPromise = sqs
+    .getQueueUrl({
+      QueueName: DLQ_QUEUE_NAME,
+    })
+    .promise()
+    .then(({QueueUrl: queueUrl}) => {
+      log.debug({queueUrl}, 'Using queue URL');
+      return queueUrl;
+    });
 
   function retry(event) {
     return queueUrlPromise.then(queueUrl => {
       return sqs
-        .receiveMessage({ QueueUrl: queueUrl, MaxNumberOfMessages: MAX_MESSAGES })
+        .receiveMessage({QueueUrl: queueUrl, MaxNumberOfMessages: MAX_MESSAGES})
         .promise()
         .then(data => {
           if (data.Messages) {
-            const count = data.Messages.length
-            return handleSqsMessages(data.Messages, { queueUrl }).then(() => ({
-              count
-            }))
+            const count = data.Messages.length;
+            return handleSqsMessages(data.Messages, {queueUrl}).then(() => ({
+              count,
+            }));
           }
-        })
-    })
+        });
+    });
   }
 
-  function handleSqsMessages(sqsMessages, { queueUrl }) {
+  function handleSqsMessages(sqsMessages, {queueUrl}) {
     if (sqsMessages.length > 0) {
-      const [message, ...rest] = sqsMessages
+      const [message, ...rest] = sqsMessages;
       const {
         Body: body,
         MessageId: messageId,
-        ReceiptHandle: receiptHandle
-      } = message
+        ReceiptHandle: receiptHandle,
+      } = message;
 
-      log.debug({ messageId }, 'Handling SQS DLQ Messges')
+      log.debug({messageId}, 'Handling SQS DLQ Messges');
 
-      const dlqEvent = JSON.parse(body)
+      const dlqEvent = JSON.parse(body);
 
       return new Promise((resolve, reject) => {
-        handler(dlqEvent, {}, err => {
-          if (err) {
-            return reject(err)
-          }
-          resolve(
-            sqs.deleteMessage({
+        const successHandler = once(result =>
+          sqs
+            .deleteMessage({
               QueueUrl: queueUrl,
-              ReceiptHandle: receiptHandle
+              ReceiptHandle: receiptHandle,
             })
-          )
-        })
-      }).then(() => handleSqsMessages(rest, { queueUrl }))
+            .promise()
+            .then(() => resolve(result)),
+        );
+
+        const errorHandler = once(err => reject(err));
+        let handlerResult;
+        try {
+          handlerResult = handler(dlqEvent, {}, (err, result) => {
+            if (err) {
+              return errorHandler(err);
+            }
+            successHandler(null, result);
+          });
+        } catch (err) {
+          return errorHandler(err);
+        }
+
+        if (handlerResult) {
+          if (handlerResult.then && handlerResult.catch) {
+            handlerResult.then(successHandler).catch(errorHandler);
+          } else {
+            successHandler(handlerResult);
+          }
+        }
+      }).then(() => handleSqsMessages(rest, {queueUrl}));
     }
   }
+
+  return retry;
 }
 
-module.exports = dlqHandlerCreator
+module.exports = dlqHandlerCreator;
